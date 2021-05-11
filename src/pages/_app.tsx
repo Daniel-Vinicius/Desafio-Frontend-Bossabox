f27b0db035b88a0261c5ab3caa564050b7391966
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-05-11 14:41:12 -0300

    feat: :sparkles: React Hook Form, Yup, Chakra UI, Axios, React Query, Remove Styled components, favicon, eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
-import { GlobalStyle } from "../../styles/global";
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { queryClient } from '../services/queryClient';
+import { theme } from '../styles/theme';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      {isDevelopment && <ReactQueryDevtools />}
+    </QueryClientProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,78 @@
-import { Container } from "./styles.home";
+import Head from 'next/head';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-import SearchBar from "../components/SearchBar";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form/Input';
+
+type SignInFormData = { email: string; password: string };
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
+export default function Home(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
+
   return (
     <>
-      <Container>
-        <header>
-          <h1>VUTTR</h1>
-          <h2>Very Useful Tools to remember</h2>
-        </header>
-        <SearchBar checkboxLabel="Search in tags only" />
-      </Container>
+      <Head>
+        <title>Desafio Bossa Box</title>
+      </Head>
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth="22.5rem"
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+          onSubmit={handleSubmit(handleSignIn)}
+        >
+          <Stack spacing="4">
+            <Input
+              label="E-mail"
+              type="email"
+              error={errors.email}
+              {...register('email')}
+            />
+            <Input
+              label="Senha"
+              type="password"
+              error={errors.password}
+              {...register('password')}
+            />
+          </Stack>
+
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
     </>
   );
 }
